daa44158c37016474e3f02266f113cc1a49de1b2
Bobby Brown <farantgh@gmail.com>
2021-03-05 15:27:04 -0500

    added the name format of user profile image

--- api/models.py
+++ api/models.py
@@ -9,17 +9,13 @@ def todo_image(instance, filename):
 
 
 def user_image(instance, filename):
-	saved_file_name = instance.user.username + datetime.now().strftime("%Y_%m_%d") + ".jpg"
+	saved_file_name = instance.user.username + "-" + datetime.now().strftime("%Y_%m_%d-%H%M%S") + ".jpg"
 	return "profile/{0}/{1}".format(instance.user.username, saved_file_name)
 
 def random_key():
 	allowed_chars = list(string.ascii_lowercase) + list(string.digits)
 	random_key = ""
 	key = random_key.join(random.sample(allowed_chars, 15))
-	todos = Todo.objects.all()
-	for todo in todos:
-		if key == todo.key:
-			raise "Error::try again later!"
 	return key
 
 class Todo(models.Model):
