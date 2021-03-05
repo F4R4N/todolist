c90810aaf0dd36f07d4697eed78f723b404e6728
Bobby Brown <farantgh@gmail.com>
2021-03-05 15:43:52 -0500

    added new features urlpath

--- auth/urls.py
+++ auth/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-from auth.views import RegisterView, ChangePasswordView, UpdateProfileView, LogoutView, UpdateUserImageView
+from auth.views import RegisterView, ChangePasswordView, UpdateProfileView, LogoutView, UpdateUserImageView, DeleteProfileView, ForgotPasswordView, ValidateConfirmationCodeView, ResetPasswordView, UserLoginView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', UserLoginView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('dashboard/change_password/<int:pk>/', ChangePasswordView.as_view(), name='auth_change_password'),
-    path('dashboard/update_profile/<int:pk>/', UpdateProfileView.as_view(), name='auth_update_profile'),
-    path('dashboard/change_image/<int:pk>/', UpdateUserImageView.as_view(), name='auth_image'),
+    path('dashboard/change_password/<key>/', ChangePasswordView.as_view(), name='auth_change_password'),
+    path('dashboard/update_profile/<key>/', UpdateProfileView.as_view(), name='auth_update_profile'),
+    path('dashboard/change_image/<key>/', UpdateUserImageView.as_view(), name='auth_image'),
     path('dashboard/logout/', LogoutView.as_view(), name='auth_logout'),
+    path('dashboard/delete_profile/<key>/', DeleteProfileView.as_view(), name='auth_delete_profile'),
+    path('forgot_password/', ForgotPasswordView.as_view(), name='auth_forgot_password'),
+    path('confirm/', ValidateConfirmationCodeView.as_view(), name='auth_confirm'),
+    path('reset_password/<key>', ResetPasswordView.as_view(), name='auth_reset_password')
+
 ]
