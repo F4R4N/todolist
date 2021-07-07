898021ce08e71c3cfb290e8dbd1d56bf644fedb0
Faran <farantgh@gmail.com>
2021-07-07 11:59:37 +0430

    add sample of needed env variables

---
+++ env.cmd
@@ -0,0 +1,12 @@
+:: environment variables for deployment
+SET CONFIG_EMAIL=example@gmail.com
+SET CONFIG_EMAIL_PASSWORD=password
+SET CONFIG_EMAIL_SMTP=smtp.google.com
+SET CONFIG_SECRET_KEY=secret_key
+SET CONFIG_DEBUG=0
+SET CONFIG_ALLOWED_HOSTS=127.0.0.1, localhost
+SET DB=db_name
+SET DB_USER=db_user
+SET DB_PASSWORD=db_password
+SET DB_HOST=db_host
+SET DB_PORT=db_port
