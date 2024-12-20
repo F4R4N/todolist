781ca94334b86a30a0190e8ce5128f626998b78a
Faran <farantgh@gmail.com>
2024-12-20 17:26:31 +0330

    edit database setting for demo deployment

--- config/settings.py
+++ config/settings.py
@@ -59,17 +59,22 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # db config
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get("DB"),
+#         'USER': os.environ.get("DB_USER"),
+#         'PASSWORD': os.environ.get("DB_PASSWORD"),
+#         'HOST': os.environ.get("DB_HOST"),
+#         'PORT': int(os.environ.get("DB_PORT")),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': int(os.environ.get("DB_PORT")),
+        'ENGINE': "django.db.backends.sqlite3",
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
